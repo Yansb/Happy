59f50d234079ddc563215652068fb96d1712ec80
Yan Santana Barreiro <yansbarreiro@gmail.com>
2020-10-12 21:45:56 -0300

    :tada: first commit

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import mapMarkerImg from '../images/local.svg';
+import { Link } from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import {Map, TileLayer} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import '../styles/pages/orphanages-map.css';
+
+
+const OrphanagesMap: React.FC = () =>{
+  return(
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita 😁</p>
+        </header>
+
+        <footer>
+          <strong>Salvador</strong>
+          <span>Bahia</span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[-12.9598135,-38.3692486]}
+        zoom={15}
+        style={{
+          width: '100%',
+          height: '100%'
+        }}
+        className='leaflet-container'
+      >
+       <TileLayer 
+        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+       />
+      </Map>
+
+      <Link to='' className='create-orphanage'>
+        <FiPlus size={32} color='#fff'/>
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
